d28a642a8d439446c06928c3ce7e1d65b7761f6d
CarolFabri <147168515+CarolFabri@users.noreply.github.com>
2025-11-11 19:17:37 +0000

    updating

--- simpleSocialStarter/index.js
+++ simpleSocialStarter/index.js
@@ -1,27 +1,44 @@
-const express= require('express')
-const app=express()
-const path=require('path')
+const express = require('express');
+const app = express();
+const path = require('path');
 
-app.listen(3000, ()=>{
-    console.log('listening on port 3000')
-})
+app.listen(3000, () => {
+  console.log('http://localhost:3000');
+});
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use(express.urlencoded({extended: false}))
+app.get('/app', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views', 'app.html'));
+});
 
-app.get('/app', (request, response)=>{
-    response.sendFile(path.join(__dirname, '/views', 'app.html'))
-})
+app.get('/getposts', (req, res) => {
+  res.json({ posts: posts });
+});
 
-app.get('/login', (request, response)=>{
-    response.sendFile(path.join(__dirname, '/views', 'login.html'))
-})
+app.post('/newpost', (req, res) => {
+  const newPost = { message: req.body.message, user: 'userX' };
+  posts.push(newPost);
+  res.sendFile(path.join(__dirname, 'views', 'app.html'));
+ 
+});
 
-app.get('/register', (request, response)=>{
-    response.sendFile(path.join(__dirname, '/views', 'register.html'))
-})
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views', 'login.html'));
+});
 
-app.get('/logout', (request, response)=>{
-    response.sendFile(path.join(__dirname, '/views', 'logout.html'))
-})
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views', 'register.html'));
+});
+
+app.get('/logout', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views', 'logout.html'));
+});
+
+let posts = [
+  { message: 'hello', user: 'user1' },
+  { message: 'goodbye', user: 'user2' }
+];
+
---
+++ simpleSocialStarter/models/posts.js
@@ -0,0 +1,29 @@
+let postData=[
+    {
+        postid: 0,
+        message:"Hi it's Dave",
+        user:"Dave"
+    },{
+        postid: 1,
+        message:"Glad it's Thursday",
+        user:"Julie"
+    }
+]
+
+function getPosts(){
+    return postData.slice()
+}
+
+function addPost(message, user){
+    let newPost={
+        postid: nextPostID++,
+        message: message,
+        user: user
+    }
+    postData.push(newPost)
+}
+
+module.exports={
+    getPosts,
+    addPost
+}
